99e4048736bdad7c59a740a5f01014a2bee3887b
Harangad Singh <s10267942@connect.np.edu.sg>
2025-08-20 21:52:43 +0800

    Added function to allow user to replay the game. Created functions to make code more dynamic and resuable. (And easier to understand).

--- week1/day1-number-guessing-game/number-guess.py
+++ week1/day1-number-guessing-game/number-guess.py
@@ -1,28 +1,47 @@
 import random # Import the random module, allowing us to use a random number generation function
 
-print("Welcome to Harangad's Number Guessing Game!") # Display a welcome message
-
 # Display the rules of the game
-print("Rules:")
-print("1. I will think of a number between 1 and 100.")
-print("2. You have to guess the number.")
-print("3. I will tell you if your guess is too high, too low, or correct.")
-print("Let's get started!")
+def display_rules():
+    print("\n Rules\n:")
+    print("1. I will think of a number between 1 and 100.")
+    print("2. You have to guess the number.")
+    print("3. I will tell you if your guess is too high, too low, or correct.")
+    print("\n Let's get started!")
 
-number_to_guess = random.randint(1, 100) # Generate a random number between 1 and 100
+def generate_number():
+    return random.randint(1, 100)
+    
+number_to_guess = generate_number()
 
-print("I have thought of a number between 1 and 100. Try to guess it!") # Prompt the user to start guessing
-player_guess = int(input("Enter your guess: ")) # Get the player's first guess
+def play_game():
+    print("\n I have thought of a number between 1 and 100. Try to guess it!") # Prompt the user to start guessing
+    player_guess = int(input("Enter your guess: ")) # Get the player's first guess
 
-# Loop until the player guesses the correct number
-while player_guess != number_to_guess:
-    if player_guess < number_to_guess: 
-        print("Your guess is too low! Try again.")
-    else:
-        print("Your guess is too high! Try again.")
-
+    # Loop until the player guesses the correct number
+    while player_guess != number_to_guess:
+        if player_guess < number_to_guess: 
+            print("Your guess is too low! Try again.")
+        else:
+            print("Your guess is too high! Try again.")
     player_guess = int(input("Enter your guess: "))
 
-# End of game
-print("Congratulations! You guessed the number correctly.")
-print("Thank you for playing!")
+    print("\n Congratulations! You've guessed the number correctly!") # Congratulate the player for guessing correctly
+    play_again() # Ask the player if they want to play again
+
+# Function to handle ending the game
+def end_game():
+    print("\n Game Over!")
+    print("Thank you for playing!")
+
+# Function to handle playing again
+def play_again():
+    response = input("\n Do you want to play again? (Yes/No): ")
+    if response.lower() == 'yes':
+        play_game()
+    else:
+        end_game()
+
+def main():
+    print("Welcome to Harangad's Number Guessing Game!") # Display a welcome message
+    display_rules()
+    play_game()

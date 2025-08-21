04c93b3965fb7e26736e90ab640c9fec69232b53
Harangad Singh <harangad2007@gmail.com>
2025-08-21 15:17:15 +0800

    Edited location of variable to fix error with the tracker variable.

--- week1/day1-number-guessing-game/number-guess.py
+++ week1/day1-number-guessing-game/number-guess.py
@@ -12,9 +12,10 @@ def generate_number():
     return random.randint(1, 100)
     
 number_to_guess = generate_number()
-attempts = 0
 
 def play_game():
+    attempts = 1
+
     print("\nI have thought of a number between 1 and 100. Try to guess it!") # Prompt the user to start guessing
     player_guess = int(input("Enter your guess: ")) # Get the player's first guess
 
@@ -22,11 +23,10 @@ def play_game():
     while player_guess != number_to_guess:
         if player_guess < number_to_guess: 
             print("Your guess is too low! Try again.")
-            attempts += 1
         else:
             print("Your guess is too high! Try again.")
-            attempts += 1
         player_guess = int(input("Enter your guess: "))
+        attempts += 1 # Increment the number of attempts after each guess
 
     print(f"\nCongratulations! You've guessed the number correctly in {attempts} attempts!") # Congratulate the player for guessing correctly
     play_again() # Ask the player if they want to play again
